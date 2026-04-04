3498bba31673d48d26c3258e709ff76e80f1ca58
Carles Rojas <carlesrojas@outlook.com>
2026-04-04 19:30:19 +0200

    remove scroll from body

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -96,7 +96,7 @@ function RootDocument({ children }: Props) {
             <HeadContent />
           </head>
           {/* bg-neutral-900 */}
-          <body className="size-full max-h-full min-h-full max-w-full min-w-full overscroll-none bg-green-500 font-sans wrap-anywhere antialiased selection:bg-sky-500/30">
+          <body className="size-full max-h-full min-h-full max-w-full min-w-full overflow-hidden overscroll-none bg-green-500 font-sans wrap-anywhere antialiased selection:bg-sky-500/30">
             <ToastProvider>
               <AppShell>{children}</AppShell>
             </ToastProvider>
