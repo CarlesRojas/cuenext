5c77c7f4c8d12e9315c62f9744d287c2bed2333f
Carles Rojas <carlesrojas@outlook.com>
2026-04-06 10:07:23 +0200

    fix param

--- src/hooks/useSearchParams.ts
+++ src/hooks/useSearchParams.ts
@@ -3,7 +3,7 @@ import { useLocation } from '@tanstack/react-router'
 
 const useSearchParams = <T extends Record<string, any> = {}>() => {
   const location = useLocation()
-  const query = location.search.query ? decodeURIComponent(location.search.query) : ''
+  const query = location.search.query ? decodeURIComponent(location.search.query) : undefined
 
   return { ...location.search, query } as UrlParams & T
 }
