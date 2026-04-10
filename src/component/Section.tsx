c9f3c8476cbb105e4c5b500741219d05d0137df0
Carles Rojas <carlesrojas@outlook.com>
2026-04-10 16:39:46 +0200

    smooth sections

--- src/component/Section.tsx
+++ src/component/Section.tsx
@@ -62,16 +62,9 @@ export function Section({
             animate={{ height: 'auto', opacity: 1 }}
             exit={{ height: 0, opacity: 0 }}
             transition={{ type: 'spring', stiffness: 300, damping: 30 }}
-            className="group relative w-full overflow-hidden"
+            className="group relative w-full overflow-hidden px-4"
           >
-            <Carousel
-              opts={{ align: 'start', dragFree: true, slidesToScroll: 'auto' }}
-              className={cn(
-                'w-full px-4',
-                !isMobile &&
-                  'pl-aside sidebar-collapsed:pl-aside-collapsed duration-slow w-dvw px-8 transition-[padding]',
-              )}
-            >
+            <Carousel opts={{ align: 'start', dragFree: true, slidesToScroll: 'auto' }} className="w-full">
               <CarouselPrevious
                 className={cn(
                   'mouse:block z-10 hidden',
@@ -80,12 +73,19 @@ export function Section({
                 )}
               />
 
-              <CarouselContent className={cn('z-0 -ml-4 pt-3 pb-8')}>
+              <CarouselContent className={cn('z-0 -ml-4 pt-3 pb-8 md:-ml-8')}>
                 {React.Children.map(
                   children,
                   (child, index) =>
                     !!child && (
-                      <CarouselItem key={index} className="basis-auto">
+                      <CarouselItem
+                        key={index}
+                        className={cn(
+                          'duration-slow basis-auto transition-[margin]',
+                          'first:md:ml-aside first:sidebar-collapsed:md:ml-aside-collapsed',
+                          'last:md:mr-aside-collapsed last:sidebar-collapsed:md:mr-aside',
+                        )}
+                      >
                         {child}
                       </CarouselItem>
                     ),
